038b77459f46ed8ef2d6d0260b80b0ffb25b7980
shubham4489 <143184073+shubham4489@users.noreply.github.com>
2024-08-24 11:46:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const container = document.getElementById("circle-container");
+
+const NumberOfCircles = 5;
+
+function createCircle(id){
+ const circle =document.createElement("div");
+	circle.classList.add('circle');
+	circle.id('circle-${id}');
+	return circle;
+ }
+ function  createLine(){
+	 const line = document.CreateElement("div");
+	 line.classList.add('line');
+	 return line;
+ }
+   for(let i=1; i<=NumberOfCircles; i++){
+	   const circle = createCircle(i);
+	   container.appendChild(circle);
+
+
+	   if(i<NumberOfCircles){
+		   const line = createLine();
+		   container.appneChild(line);
+		   
+	   }
+   }
